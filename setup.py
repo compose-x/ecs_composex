edea15ba665254126d8825cd420d58b760625313
John Preston <JohnPreston@users.noreply.github.com>
2020-04-17 01:58:29 +0100

    Added `make conform` to run black against the code to standardize syntax (#26)
    Modified tox configuration to include requirements.txt
    Added requirements files to the package
    Requirements packages list in setup.py from requirements.txt

--- setup.py
+++ setup.py
@@ -3,57 +3,64 @@
 
 """The setup script."""
 
-from setuptools import setup, Extension, find_packages
+import os
+from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+DIR_HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(f"{DIR_HERE}/README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open(f"{DIR_HERE}/HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [
-    'troposphere==2.6.0',
-    'boto3==1.12.36'
-]
+requirements = []
+with open(f"{DIR_HERE}/requirements.txt", "r") as req_fd:
+    for line in req_fd:
+        requirements.append(line.strip())
 
-setup_requirements = ['pytest-runner>=5.1']
-test_requirements = ['pytest>=4.6', "tox==3.14.6"]
+test_requirements = []
+with open(f"{DIR_HERE}/requirements_test.txt", "r") as req_fd:
+    for line in req_fd:
+        test_requirements.append(line.strip())
+
+setup_requirements = []
 
 setup(
-    author="John Mille",
-    author_email='john@lambda-my-aws.io',
-    python_requires='!=2.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    author="John Preston",
+    author_email="john@lambda-my-aws.io",
+    python_requires="!=2.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
     ],
     description="Package that generates CFN templates based on a Docker Compose file to generate ECS Cluster,"
-                "ECS Services and Extra resources such as SQS Queues, SNS Topics etc. that can be accessed via IAM",
+    "ECS Services and Extra resources such as SQS Queues, SNS Topics etc. that can be accessed via IAM",
     entry_points={
-        'console_scripts': [
-            'ecs_composex=ecs_composex.cli:main',
-            'ecs_composex-compute=ecs_composex.compute.cli:main',
-            'ecs_composex-vpc=ecs_composex.vpc.cli:main',
-            'ecs_composex-sqs=ecs_composex.sqs.cli:main'
+        "console_scripts": [
+            "ecs_composex=ecs_composex.cli:main",
+            "ecs_composex-compute=ecs_composex.compute.cli:main",
+            "ecs_composex-vpc=ecs_composex.vpc.cli:main",
+            "ecs_composex-sqs=ecs_composex.sqs.cli:main",
         ]
     },
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='ecs_composex',
-    name='ecs_composex',
-    packages=find_packages(include=['ecs_composex', 'ecs_composex.*']),
+    keywords="ecs_composex",
+    name="ecs_composex",
+    packages=find_packages(include=["ecs_composex", "ecs_composex.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/lambda-my-aws/ecs_composex',
-    version='0.1.3',
+    url="https://github.com/lambda-my-aws/ecs_composex",
+    version="0.1.3",
     zip_safe=False
 )
