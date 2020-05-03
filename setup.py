d43ac6f877662a0e7ccd017010bb3fe83f069c57
John Preston <john@lambda-my-aws.io>
2020-05-03 19:04:14 +0100

    Bump version: 0.2.0 → 0.2.1
    Patch CLI and CFN tool to generate the CFN configuration file.

--- setup.py
+++ setup.py
@@ -78,6 +78,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lambda-my-aws/ecs_composex",
-    version="0.2.0",
+    version="0.2.1",
     zip_safe=False,
 )
