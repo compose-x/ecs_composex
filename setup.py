656c520066d0c93826a9a7678829ee7e0f3b86de
John Preston <JohnPreston@users.noreply.github.com>
2020-06-21 23:01:19 +0100

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -84,6 +84,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lambda-my-aws/ecs_composex",
-    version="0.2.3",
+    version="0.3.0",
     zip_safe=False,
 )
