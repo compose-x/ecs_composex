cf22ccd02d40204d0a75f940cbd4180f2be383db
John Preston <JohnPreston@users.noreply.github.com>
2020-08-07 17:32:21 +0100

    Changelog

--- ecs_composex/ecs/ecs_service.py
+++ ecs_composex/ecs/ecs_service.py
@@ -239,7 +239,9 @@ class Task(object):
                 service_config["definition"],
                 service_config["config"],
             )
-            import_secrets(template, service_config["definition"], container.definition, settings)
+            import_secrets(
+                template, service_config["definition"], container.definition, settings
+            )
             self.containers.append(container.definition)
             self.stack_parameters.update(container.stack_parameters)
             if self.family_config is None:
