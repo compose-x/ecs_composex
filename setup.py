29236a800a509393ec94cdb0e219ccad4e19a0b1
John Preston <john@lambda-my-aws.io>
2020-10-20 09:18:04 +0100

    Bump version: 0.8.3 → 0.8.4

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lambda-my-aws/ecs_composex",
-    version="0.8.3",
+    version="0.8.4",
     zip_safe=False,
 )
