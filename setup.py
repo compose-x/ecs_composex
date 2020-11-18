6e4ecb46645df69cdcc79e2b63484faf90c580e8
John Preston <john@lambda-my-aws.io>
2020-11-18 23:39:37 +0000

    Patched version of 0.8.9 with previews for 0.9.0 Docs updated to align with the upcoming 0.9.0 release
    Bump version: 0.8.8 → 0.8.9

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lambda-my-aws/ecs_composex",
-    version="0.8.8",
+    version="0.8.9",
     zip_safe=False,
 )
