213e517fd5090ffffc8184c43a78d590fc885528
John Preston <john@ews-network.net>
2021-02-17 17:55:26 +0000

    Adding parameter for DNS Private Namespace for service stack

--- ecs_composex/ecs/ecs_template.py
+++ ecs_composex/ecs/ecs_template.py
@@ -66,6 +66,7 @@ def initialize_service_template(service_name):
             dns_params.PRIVATE_DNS_ZONE_NAME,
             dns_params.PUBLIC_DNS_ZONE_ID,
             dns_params.PRIVATE_DNS_ZONE_ID,
+            dns_params.PRIVATE_NAMESPACE_ID,
             ecs_params.CLUSTER_NAME,
             ecs_params.LAUNCH_TYPE,
             ecs_params.ECS_CONTROLLER,
