81e1f121269ee431dc64b1d1f30858ffa413c79c
John Preston <john@ews-network.net>
2021-02-17 16:04:19 +0000

    Adding the ZoneID to Template for ease of use

--- ecs_composex/dns/dns_lookup.py
+++ ecs_composex/dns/dns_lookup.py
@@ -79,8 +79,9 @@ def lookup_service_discovery_namespace(zone, session, private):
                 "Only DNS namespaces, private or public, are supported"
             )
         return {
-            "ZoneId": the_zone["Id"],
+            "Route53ID": properties["DnsProperties"]["HostedZoneId"],
             "ZoneTld": LAST_DOT_RE.sub("", properties["HttpProperties"]["HttpName"]),
+            "ZoneId": zone_r["Namespace"]["Id"],
         }
     except client.exceptions.NamespaceNotFound:
         LOG.error(f"Namespace not found for {zone.name}")
