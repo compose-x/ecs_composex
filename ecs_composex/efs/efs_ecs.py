a96291639cf6f82edce58d794afe0281cf026f85
John Preston <john@ews-network.net>
2021-02-27 08:28:18 +0000

    Correcting code-smells

--- ecs_composex/efs/efs_ecs.py
+++ ecs_composex/efs/efs_ecs.py
@@ -16,15 +16,48 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from troposphere import Ref, GetAtt
-from troposphere import Parameter
 from troposphere.ecs import EFSVolumeConfiguration, Volume, AuthorizationConfig
 from troposphere.efs import AccessPoint, PosixUser, RootDirectory, CreationInfo
-from troposphere.iam import Policy, PolicyType
+from troposphere.iam import PolicyType
 
 from ecs_composex.common import add_parameters, keyisset
 from ecs_composex.ecs.ecs_params import TASK_T, TASK_ROLE_T, SERVICE_T
-from ecs_composex.tcp_resources_settings import handle_new_tcp_resource
 from ecs_composex.efs.efs_params import FS_PORT, FS_MNT_PT_SG_ID, FS_ID, FS_ARN
+from ecs_composex.tcp_resources_settings import handle_new_tcp_resource
+
+
+def get_volumes(task_definition):
+    """
+    Function to fetch the volumes from the task definition
+
+    :param troposphere.ecs.TaskDefinition task_definition:
+    :return: the volumes list of the task definition or new ones
+    :rtype: list
+    """
+    volumes = (
+        getattr(task_definition, "Volumes")
+        if (hasattr(task_definition, "Volumes"))
+        else []
+    )
+    if not volumes:
+        setattr(task_definition, "Volumes", volumes)
+    return volumes
+
+
+def get_service_mount_points(service):
+    """
+    Function to get the MountPoints from the container definition of a service
+
+    :param ecs_composex.common.compose_services.ComposeService service:
+    :return: list of mount points or new list
+    :rtype: list
+    """
+    mount_points = []
+    if not hasattr(service.container_definition, "MountPoints"):
+        setattr(service.container_definition, "MountPoints", mount_points)
+    else:
+        mount_points = getattr(service.container_definition, "MountPoints")
+    return mount_points
 
 
 def add_task_iam_access_to_access_point(family, access_points, efs):
@@ -99,11 +132,7 @@ def override_service_volume(new_efs, fs_id, target, access_points, volumes):
     for service in target[2]:
         if not service.user:
             continue
-        mount_points = []
-        if not hasattr(service.container_definition, "MountPoints"):
-            setattr(service.container_definition, "MountPoints", mount_points)
-        else:
-            mount_points = getattr(service.container_definition, "MountPoints")
+        mount_points = get_service_mount_points(service)
         for count, mount_pt in enumerate(mount_points):
             if mount_pt.SourceVolume == new_efs.volume.volume_name:
                 container_volume_name = (
@@ -151,11 +180,7 @@ def set_user_to_access_points(efs, fs_id, access_points, service):
     Function to set the PosixUser to a specific access point for a specific given service
     """
     group_id = service.group if service.group else service.user
-    mount_points = []
-    if not hasattr(service.container_definition, "MountPoints"):
-        setattr(service.container_definition, "MountPoints", mount_points)
-    else:
-        mount_points = getattr(service.container_definition, "MountPoints")
+    mount_points = get_service_mount_points(service)
     for mount_pt in mount_points:
         if mount_pt.SourceVolume == efs.volume.volume_name:
             for access_point in access_points:
@@ -172,6 +197,37 @@ def set_user_to_access_points(efs, fs_id, access_points, service):
                         Path=mount_pt.ContainerPath,
                     ),
                 ),
+
+
+def override_efs_settings(new_efs, target, fs_id_parameter, access_points, volumes):
+    """
+    Function to determine if access points should be set on a per service of the task definition
+    and update the volumes and mount points accordingly
+
+    :param ecs_composex.efs.efs_stack.Efs new_efs:
+    :param tuple target:
+    :param ecs_composex.common.cfn_params.Parameter fs_id_parameter:
+    :param list access_points:
+    :param list volumes:
+    :return:
+    """
+    if [service.user for service in target[2]] and len(
+        [service.user for service in target[2]]
+    ) > 1:
+        override_service_volume(
+            new_efs, fs_id_parameter, target, access_points, volumes
+        )
+    elif [service.user for service in target[2]] and len(
+        [service.user for service in target[2]]
+    ) == 1:
+        for service in target[2]:
+            if service.user:
+                set_user_to_access_points(
+                    new_efs,
+                    fs_id_parameter,
+                    access_points,
+                    service,
+                )
 
 
 def expand_family_with_efs_volumes(efs_root_stack_title, new_efs, settings):
@@ -216,31 +272,9 @@ def expand_family_with_efs_volumes(efs_root_stack_title, new_efs, settings):
             EFSVolumeConfiguration=EFSVolumeConfiguration(**efs_config_kwargs),
             Name=new_efs.volume.volume_name,
         )
-        volumes = (
-            getattr(task_definition, "Volumes")
-            if (hasattr(task_definition, "Volumes"))
-            else []
-        )
-        if not volumes:
-            setattr(task_definition, "Volumes", volumes)
+        volumes = get_volumes(task_definition)
         volumes.append(efs_volume_definition)
-        if [service.user for service in target[2]] and len(
-            [service.user for service in target[2]]
-        ) > 1:
-            override_service_volume(
-                new_efs, fs_id_parameter, target, access_points, volumes
-            )
-        elif [service.user for service in target[2]] and len(
-            [service.user for service in target[2]]
-        ) == 1:
-            for service in target[2]:
-                if service.user:
-                    set_user_to_access_points(
-                        new_efs,
-                        fs_id_parameter,
-                        access_points,
-                        service,
-                    )
+        override_efs_settings(new_efs, target, fs_id_parameter, access_points, volumes)
         add_task_iam_access_to_access_point(target[0], access_points, new_efs)
 
 
@@ -255,12 +289,8 @@ def efs_to_ecs(resources, services_stack, res_root_stack, settings):
     :return:
     """
 
-    efs_mappings = {}
     new_resources = [
         resources[res_name] for res_name in resources if not resources[res_name].lookup
-    ]
-    lookup_resources = [
-        resources[res_name] for res_name in resources if resources[res_name].lookup
     ]
     for new_res in new_resources:
         handle_new_tcp_resource(
