376cee587314e7a6d98dc108005c7cf6657c20dd
John Preston <john@ews-network.net>
2021-05-13 10:09:09 +0100

    Bump to 0.15.0

--- setup.py
+++ setup.py
@@ -76,6 +76,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/compose-x/ecs_composex",
-    version="0.14.4",
+    version="0.15.0",
     zip_safe=False,
 )
