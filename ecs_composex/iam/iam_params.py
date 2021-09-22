292fffc8a06bbe3d1360f35c58e892dfcc9c108d
John Preston <john@ews-network.net>
2021-09-22 14:33:44 +0100

    WIP

---
+++ ecs_composex/iam/iam_params.py
@@ -0,0 +1,37 @@
+﻿#   -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: MPL-2.0
+#  Copyright 2020-2021 John Mille <john@compose-x.io>
+
+import re
+from os import path
+
+from troposphere import Ref, Sub
+
+from ecs_composex.common import NONALPHANUM
+from ecs_composex.common.cfn_params import Parameter
+from ecs_composex.common.ecs_composex import X_KEY
+
+MOD_KEY = path.basename(path.dirname(path.abspath(__file__)))
+RES_KEY = f"{X_KEY}{MOD_KEY}"
+MAPPINGS_KEY = NONALPHANUM.sub("", MOD_KEY)
+
+ROLE_ID_RE = re.compile(r"^[A-Z0-9]+$")
+
+POLICY_RE = re.compile(
+    r"((^([a-zA-Z0-9-_./]+)$)|(^(arn:aws:iam::(aws|\d{12}):policy/)[a-zA-Z0-9-_./]+$))"
+)
+
+
+IAM_ROLE_T = "IamRoleName"
+IAM_ROLE = Parameter(IAM_ROLE_T, return_value=None, Type="String")
+
+IAM_ROLE_ARN_T = "IamRoleArn"
+IAM_ROLE_ARN = Parameter(IAM_ROLE_ARN_T, return_value="Arn", Type="String")
+
+IAM_ROLE_ID_T = "IamRoleId"
+IAM_ROLE_ID = Parameter(
+    IAM_ROLE_ID_T,
+    return_value="RoleId",
+    Type="String",
+    AllowedPattern=ROLE_ID_RE.pattern,
+)
---
+++ ecs_composex/iam/iam_stack.py
@@ -0,0 +1,156 @@
+﻿#   -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: MPL-2.0
+#  Copyright 2020-2021 John Mille <john@compose-x.io>
+
+
+from compose_x_common.compose_x_common import keyisset
+from troposphere import GetAtt, Ref, Sub
+from troposphere.iam import Policy, PolicyType
+from troposphere.iam import Role as IamRole
+
+from ecs_composex.common import build_template
+from ecs_composex.common.stacks import ComposeXStack
+from ecs_composex.ecs.ecs_params import CLUSTER_NAME_T
+from ecs_composex.iam import service_role_trust_policy
+from ecs_composex.iam.iam_params import IAM_ROLE, IAM_ROLE_ARN, IAM_ROLE_ID
+
+
+class EcsRole(object):
+    """
+    Class to wrap around the AWS IAM Role
+    """
+
+    def __init__(self, family, role_type):
+        """
+        :param family: The family the role will belong to
+        """
+        if role_type not in ["TaskRole", "ExecRole"]:
+            raise ValueError(
+                "role_type is", role_type, "expected one of", ["TaskRole", "ExecRole"]
+            )
+        self.family = family
+        self.logical_name = f"{self.family.logical_name}{role_type}"
+        self.cfn_resource = None
+        self.init_role(role_type)
+
+        self.output_properties = {
+            IAM_ROLE: (self.logical_name, self.cfn_resource, Ref, None),
+            IAM_ROLE_ID: (
+                f"{self.logical_name}{IAM_ROLE_ID.return_value}",
+                self.cfn_resource,
+                GetAtt,
+                IAM_ROLE_ID.return_value,
+            ),
+            IAM_ROLE_ARN: (
+                f"{self.logical_name}{IAM_ROLE_ARN.return_value}",
+                self.cfn_resource,
+                GetAtt,
+                IAM_ROLE_ARN.return_value,
+            ),
+        }
+        self.attributes_outputs = {}
+
+    def init_role(self, role_type):
+        if role_type == "ExecRole":
+            self.cfn_resource = IamRole(
+                self.logical_name,
+                AssumeRolePolicyDocument=service_role_trust_policy("ecs-tasks"),
+                Description=Sub(
+                    f"Execution role for {self.family.logical_name} in ${{{CLUSTER_NAME_T}}}"
+                ),
+                ManagedPolicyArns=[],
+                Policies=[
+                    Policy(
+                        PolicyName=Sub("EcsExecRole"),
+                        PolicyDocument={
+                            "Version": "2012-10-17",
+                            "Statement": [
+                                {
+                                    "Sid": "AllowsForEcrPullFromEcsAgent",
+                                    "Effect": "Allow",
+                                    "Action": [
+                                        "ecr:GetAuthorizationToken",
+                                        "ecr:BatchCheckLayerAvailability",
+                                        "ecr:GetDownloadUrlForLayer",
+                                        "ecr:GetRepositoryPolicy",
+                                        "ecr:DescribeRepositories",
+                                        "ecr:ListImages",
+                                        "ecr:DescribeImages",
+                                        "ecr:BatchGetImage",
+                                    ],
+                                    "Resource": ["*"],
+                                },
+                                {
+                                    "Sid": "AllowEcsAgentOrientedTasks",
+                                    "Effect": "Allow",
+                                    "Action": [
+                                        "ecs:DiscoverPollEndpoint",
+                                        "ecs:Poll",
+                                        "ecs:Submit*",
+                                    ],
+                                    "Resource": ["*"],
+                                },
+                                {
+                                    "Sid": "AllowsEcsAgentToPerformActionsForMicroservice",
+                                    "Effect": "Allow",
+                                    "Action": [
+                                        "ec2:AttachNetworkInterface",
+                                        "ec2:CreateNetworkInterface",
+                                        "ec2:CreateNetworkInterfacePermission",
+                                        "ec2:DeleteNetworkInterface",
+                                        "ec2:DeleteNetworkInterfacePermission",
+                                        "ec2:Describe*",
+                                        "ec2:DetachNetworkInterface",
+                                        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                                        "elasticloadbalancing:DeregisterTargets",
+                                        "elasticloadbalancing:Describe*",
+                                        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                                        "elasticloadbalancing:RegisterTargets",
+                                    ],
+                                    "Resource": ["*"],
+                                },
+                            ],
+                        },
+                    )
+                ],
+            )
+        elif role_type == "TaskRole":
+            self.cfn_resource = IamRole(
+                self.logical_name,
+                AssumeRolePolicyDocument=service_role_trust_policy("ecs-tasks"),
+                Description=Sub(
+                    f"TaskRole - {self.family.logical_name} in ${{{CLUSTER_NAME_T}}}"
+                ),
+                ManagedPolicyArns=[],
+                Policies=[],
+            )
+
+    def set_role_parameters(self, role_stack):
+        pass
+
+
+def create_new_roles(settings):
+    """
+
+    :param ecs_composex.common.settings.ComposeXSettings settings:
+    """
+    roles = []
+    for family in settings.families:
+        family.task_role = EcsRole(family, "TaskRole")
+        family.exec_role = EcsRole(family, "ExecRole")
+        roles.append(family.task_role)
+        roles.append(family.exec_role)
+    return roles
+
+
+class XStack(ComposeXStack):
+    """
+    Class to represent the IAM top stack
+    """
+
+    def __init__(self, name, settings, **kwargs):
+        stack_template = build_template("Root stack for IAM Roles")
+        super().__init__(name, stack_template, **kwargs)
+        new_roles = create_new_roles(settings)
+        for role in new_roles:
+            self.stack_template.add_resource(role)
