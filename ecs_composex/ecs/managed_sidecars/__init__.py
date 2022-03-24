4ce25d93b4f793a917652951b37e587147fd6395
John Preston <john@ews-network.net>
2022-03-24 23:58:53 +0000

    Split refactor of ecs_prometheus and sidecars

---
+++ ecs_composex/ecs/managed_sidecars/__init__.py
@@ -0,0 +1,64 @@
+#   -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: MPL-2.0
+#  Copyright 2020-2022 John Mille <john@compose-x.io>
+
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ecs_composex.ecs.ecs_family import ComposeFamily
+
+from ecs_composex.common import LOG, add_parameters
+from ecs_composex.compose.compose_services import ComposeService
+
+
+class ManagedSidecar(ComposeService):
+    def __init__(self, name, definition):
+        super().__init__(name, definition)
+        self.is_essential = False
+        self.is_aws_sidecar = True
+
+    def add_to_family(self, family: ComposeFamily, is_dependency: bool = False) -> None:
+        """
+        Adds the container as a sidecar to the family in order to fulfil a specific purpose
+        for an AWS Feature, here, add xray-daemon for dynamic tracing.
+
+        :param ecs_composex.ecs.ecs_family.ComposeFamily family:
+        :param bool is_dependency: Whether the family services depend on sidecar or not.
+        """
+        self.my_family = family
+        family.add_managed_sidecar(self)
+        self.set_parameters()
+        self.set_as_dependency_to_family_services(is_dependency)
+
+    def set_parameters(self) -> None:
+        """
+        Auto adds the sidecar container image as parameter to the stack
+        """
+        print(self.name, self.my_family.name, self.image_param.title)
+        if self.my_family.template and self.my_family.stack and self.image_param:
+            add_parameters(self.my_family.template, [self.image_param])
+
+    def set_as_dependency_to_family_services(self, is_dependency: bool = False) -> None:
+        """
+        Function to update the depends_on list for the family main services
+
+        :param bool is_dependency: Whether the family services depend on sidecar or not.
+        """
+        for service in self.my_family.ordered_services:
+            if service.is_aws_sidecar:
+                continue
+            if is_dependency:
+                if service.depends_on and self.name not in service.depends_on:
+                    service.depends_on.append(self.name)
+                    LOG.info(
+                        f"{self.my_family.name}.{self.name} - Added {self.name} as dependency"
+                    )
+            else:
+                if service.name not in self.depends_on:
+                    self.depends_on.append(service.name)
+                    LOG.info(
+                        f"{self.my_family.name}.{self.name} - Added as a dependency of {self.name}"
+                    )
---
+++ ecs_composex/ecs/managed_sidecars/nginx_prometheus_exporter.py
@@ -0,0 +1,44 @@
+#   -*- coding: utf-8 -*-
+#   -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: MPL-2.0
+#  Copyright 2020-2022 John Mille <john@compose-x.io>
+
+"""
+Simple class to manage AWS XRay sidecar
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+from ecs_composex.common.cfn_params import Parameter
+
+from . import ManagedSidecar
+
+NGINX_EXPORTER_IMAGE_PARAMETER = Parameter(
+    "NginxPrometheusExporterImage",
+    Type="String",
+    Default="public.ecr.aws/nginx/nginx-prometheus-exporter:latest",
+)
+
+NGINX_EXPORTER_NAME = "cloudwatch-agent"
+NGINX_EXPORTER_DEFINITION = {
+    "image": NGINX_EXPORTER_IMAGE_PARAMETER.Default,
+    "deploy": {
+        "resources": {"limits": {"cpus": 0.1, "memory": "64M"}},
+    },
+    "labels": {
+        "container_name": "nginx-prometheus-exporter",
+        "ECS_PROMETHEUS_EXPORTER_PORT": 9113,
+        "job": "nginx-prometheus-exporter",
+    },
+    "ports": [{"target": 9113, "protocol": "tcp"}],
+}
+
+NGINX_EXPORTER_SERVICE = ManagedSidecar(
+    NGINX_EXPORTER_NAME,
+    NGINX_EXPORTER_DEFINITION,
+)
